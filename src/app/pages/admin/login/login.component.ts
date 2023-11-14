8de33bd7bcd650b05ed2dd3b8b796562daf1b24c
Julien Noel <juliennoel998@gmail.com>
2023-11-14 16:32:37 +0100

    admin base commit

--- src/app/pages/admin/login/login.component.ts
+++ src/app/pages/admin/login/login.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  
 }
