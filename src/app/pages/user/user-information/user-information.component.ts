f79b5667560630fbb26e02105fcfd70fba9fe0d0
FaseGk <giuliani.nikos@gmail.com>
2023-11-15 15:22:37 +0100

    structure forms

--- src/app/pages/user/user-information/user-information.component.ts
+++ src/app/pages/user/user-information/user-information.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder,FormGroup,Validators} from '@angular/forms';
+import { FormBuilder,FormGroup,FormsModule,ReactiveFormsModule,Validators} from '@angular/forms';
 @Component({
   selector: 'app-user-information',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule],
   templateUrl: './user-information.component.html',
   styleUrl: './user-information.component.scss'
 })
 export class UserInformationComponent {
-  registerForm : FormGroup;
+  infoForm : FormGroup;
 
   constructor(private _fb : FormBuilder){
-    this.registerForm = this._fb.group({})
+    this.infoForm = this._fb.group({
+      
+      firstName : [null, [Validators.required,Validators.minLength(2)]],
+      lastName : [null, [Validators.required]],
+      email : [null,[Validators.required]],
+      adresse : [null,Validators.required],
+      postal : [null,Validators.required],
+      town : [null,Validators.required]
+      
+    })
   }
 }
