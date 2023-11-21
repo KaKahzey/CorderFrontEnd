157600e4cceb51a27ff66c4b3db0d26d0f85c73d
xLiZZaRRdd <yeansennerenaud@hotmail.com>
2023-11-21 16:43:39 +0100

    Mise en place structure principale de la feature, modifs à faire responsive

--- src/app/pages/user/photo-information/photo-information.component.ts
+++ src/app/pages/user/photo-information/photo-information.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PesticideChoiceService } from '../../../shared/services/pesticide-choice.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-photo-information',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './photo-information.component.html',
   styleUrl: './photo-information.component.scss'
 })
