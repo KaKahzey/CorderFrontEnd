5d35f88e2988bafb501996411e8d2ae5fdc5cc43
Julien Noel <juliennoel998@gmail.com>
2023-11-22 18:17:36 +0100

    continuation dashboard

--- src/app/pages/admin/statistics/statistics.component.ts
+++ src/app/pages/admin/statistics/statistics.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarService } from '../../../shared/services/navbar.service';
 
 @Component({
   selector: 'app-statistics',
