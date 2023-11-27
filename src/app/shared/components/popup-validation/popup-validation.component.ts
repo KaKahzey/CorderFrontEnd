fd3cd2a4255cb7c164c1ac3e244e2d987d840955
Joppart Logan <joppartl@gmail.com>
2023-11-27 16:32:59 +0100

    popup starting

---
+++ src/app/shared/components/popup-validation/popup-validation.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-popup-validation',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './popup-validation.component.html',
+  styleUrl: './popup-validation.component.scss'
+})
+export class PopupValidationComponent {
+
+}
