0bf3c219d381b2834df0d158c665488d5596461e
ShynOMic <joppartl@gmail.com>
2023-11-28 16:26:18 +0100

    add the card and his principal content

--- src/app/shared/models/userData.ts
+++ src/app/shared/models/userData.ts
@@ -2,14 +2,14 @@ export interface UserData {
     firstName : string,
     lastName : string,
     email : string,
-    pictureName : string,
-    pictureType : string,
+    pictureName? : string,
+    pictureType? : string,
     blob : [1,0,1], // voir fonction pour convert canal frontend
     street : string,
     city : string,
     postCode : number,
     productUsed : string,
     newsletter: boolean,
-    shipped : boolean,
-    validated : boolean
+    shipped? : boolean,
+    validated? : boolean
 }
