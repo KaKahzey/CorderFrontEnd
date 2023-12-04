888cd66ca51e6e4fc982b170e277db5806578864
FaseGk <giuliani.nikos@gmail.com>
2023-12-04 12:05:54 +0100

    structure photo-capture-choice et remaniement pop-up

--- src/app/pages/user/thanks/thanks.component.ts
+++ src/app/pages/user/thanks/thanks.component.ts
@@ -17,9 +17,15 @@ export class ThanksComponent {
 
   ngOnInit(): void {
 
+
+    const popup0 = document.getElementById('popup0');
+    if(popup0){
+     popup0.style.display = 'block';
+    }
+
     const popup1 = document.getElementById('popup1');
     if(popup1){
-     popup1.style.display = 'block';
+     popup1.style.display = 'none';
     }
 
     const popup2 = document.getElementById('popup2');
