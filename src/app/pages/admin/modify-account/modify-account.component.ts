99af63c92fa18154c9d3c6265cab9584d765e4cb
ShynOMic <joppartl@gmail.com>
2023-12-07 15:34:50 +0100

    adding forms

--- src/app/pages/admin/modify-account/modify-account.component.ts
+++ src/app/pages/admin/modify-account/modify-account.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../shared/services/auth.service';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ApiService } from '../../../shared/services/api.service';
 
 @Component({
   selector: 'app-modify-account',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './modify-account.component.html',
   styleUrl: './modify-account.component.scss'
 })
 export class ModifyAccountComponent {
 
+  modifyForm : FormGroup
+
+  constructor(private _authService : AuthService,private _apiService : ApiService, private _router : Router, private _fb : FormBuilder){
+    this.modifyForm = this._fb.group({
+      oldPassword : [null, [Validators.required]],
+      newPassword : [null, [Validators.required]],
+      newPasswordRepeat: [null,[Validators.required]]
+    })
+  }
+  modifyPassword(){
+    
+  }
 }
